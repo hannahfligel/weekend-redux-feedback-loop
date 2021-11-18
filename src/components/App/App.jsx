72474781a1c1fb734ac17a3e04db6e4036d34150
Hannah Fligel <fligelhannah@gmail.com>
2021-11-18 16:22:12 -0600

    router created for all components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,17 +7,23 @@ import Understanding from '../Understanding/Understanding';
 import Supported from '../Supported/Supported';
 import Comments from '../Comments/Comments';
 import ThankYou from '../ThankYou/ThankYou';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 
 function App() {
 
   return (
     <div className='App'>
       <Header/>
-      <Feeling />
-      <Understanding />
-      <Supported />
-      <Comments />
-      <ThankYou/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Feeling />}></Route>
+          <Route path="/understanding" element={<Understanding />}></Route>
+          <Route path="/supported" element={<Supported />}></Route>
+          <Route path="/comments" element={<Comments/>}></Route>
+          <Route path="/thankyou" element={<ThankYou/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
