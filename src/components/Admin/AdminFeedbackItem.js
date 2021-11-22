f588165d9b1702433a088419e95762d1dd668de7
Hannah Fligel <fligelhannah@gmail.com>
2021-11-22 17:11:13 -0600

    admin created and reciving data from db

---
+++ src/components/Admin/AdminFeedbackItem.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+function AdminFeedbackItem( props ){
+    //const reducerName = useSelector( store => store.reducerName );
+    //const [name, setName] =useState ( null );
+
+    const dispatch = useDispatch();
+
+    return(
+        <tr>
+            <td>{props.feedback.feeling}</td>
+            <td>{props.feedback.understanding}</td>
+            <td>{props.feedback.support}</td>
+            <td>{props.feedback.comments}</td>
+        </tr>
+    )
+}
+
+export default AdminFeedbackItem;
